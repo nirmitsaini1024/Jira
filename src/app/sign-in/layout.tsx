4d95db45689a6a3f3810fd75cb69e808e1b58ef5
Nirmit Saini <nirmitsaini24@gmail.com>
2024-10-10 22:40:14 +0530

    first commit

---
+++ src/app/sign-in/layout.tsx
@@ -0,0 +1,9 @@
+interface SignInLayoutProps {
+  children: React.ReactNode;
+}
+
+const SignInLayout = ({ children }: SignInLayoutProps) => {
+  return <div>{children}</div>;
+};
+
+export default SignInLayout;
---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,9 @@
+const SignInPage = () => {
+    return (
+        <div>
+            Signin page
+        </div>
+      );
+}
+ 
+export default SignInPage;
